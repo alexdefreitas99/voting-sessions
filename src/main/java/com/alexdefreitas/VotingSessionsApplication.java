9a9c9a7ce274515e3e775f580bf243518096ab76
Alex de Freitas <alexdefreitas99@hotmail.com>
2020-03-16 20:40:27 -0300

    First commit

---
+++ src/main/java/com/alexdefreitas/VotingSessionsApplication.java
@@ -0,0 +1,13 @@
+package com.alexdefreitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexdefreitas/VotingSessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdefreitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
