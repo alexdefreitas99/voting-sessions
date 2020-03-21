abc18e2bb181af98281775586125f9a59538b266
Alex de Freitas <alexdefreitas99@hotmail.com>
2020-03-21 14:48:46 -0300

    Db COnfig

--- src/main/java/com/alexdefreitas/VotingSessionsApplication.java
+++ src/main/java/com/alexdefreitas/VotingSessionsApplication.java
@@ -1,9 +1,13 @@
 package com.alexdefreitas;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.alexdefreitas")
 public class VotingSessionsApplication {
 
 	public static void main(String[] args) {
