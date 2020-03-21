8d11e7d2a2b97048d5b4b5a78e08817fca61d5b3
Alex de Freitas <alexdefreitas99@hotmail.com>
2020-03-21 17:36:18 -0300

    Add session opening date

--- voting-sessions-domain/src/main/java/com/alexdefreitas/session/model/entity/SessionEntity.java
+++ voting-sessions-domain/src/main/java/com/alexdefreitas/session/model/entity/SessionEntity.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -23,4 +25,10 @@ public class SessionEntity {
     @ManyToOne
     @JoinColumn(name = "ID_AGENDA")
     private AgendaEntity agenda;
+
+    @Column(name = "OPENING_DATE")
+    @CreationTimestamp
+    private LocalDateTime openingDate;
+
+
 }
