3a7521ed044e3a44d7f755c254508b5af8003ce6
Alex de Freitas <alexdefreitas99@hotmail.com>
2020-03-22 18:01:32 -0300

    Queue and job basics

--- src/main/java/com/alexdefreitas/VotingSessionsApplication.java
+++ src/main/java/com/alexdefreitas/VotingSessionsApplication.java
@@ -1,17 +1,15 @@
 package com.alexdefreitas;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.alexdefreitas")
+@EnableScheduling
 public class VotingSessionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VotingSessionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSessionsApplication.class, args);
+    }
 
 }
